b2de8c5b3aa829b9f10f5774bfb420ca96a5ad05
imagedt_xindemeng <dingbang@imagedt.com>
2017-12-22 20:25:09 +0800

    初始化

---
+++ src/main/java/com/meng/helloconfig/HelloConfigApplication.java
@@ -0,0 +1,12 @@
+package com.meng.helloconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloConfigApplication.class, args);
+	}
+}
